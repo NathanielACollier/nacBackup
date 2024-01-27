9e8f361157059a7768d8cf6d1e1175aa21ad04a3
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-01-26 18:12:17 -0600

    Setup the deploy

---
+++ nacBackupWPF/deploy-Release.ps1
@@ -0,0 +1,12 @@
+﻿
+$deployPath = "$($env:USERPROFILE)\programs\nacBackup"
+$buildConfig = "Release"
+
+# clear existing folder
+remove-item ([system.io.path]::Combine($deployPath, "*")) -Recurse -Force
+
+
+& dotnet @("publish","-c", $buildConfig, "-o", "$deployPath", "-p:PublishSingleFile=true",
+"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true",
+"-nowarn:nu1605"
+)
